1a5d1a0efec68135f42d52c57fcc06d9a7e18705
Samir Standnes Abdi <samirstandnesabdi@Samirs-MacBook-Pro.local>
2018-12-18 01:59:12 +0530

    mvp 0.5

--- markov_python-master/fetch_data.py
+++ markov_python-master/fetch_data.py
@@ -1,40 +1,56 @@
 from bs4 import BeautifulSoup
-import urllib2
-from cc_markov import MarkovChain
-#from io import BytesIO
-#from gtts import gTTS
-import facebook
-import requests
-#import ssl
 from random import randint
+import urllib2
 import re
 
-books = ['http://www.gutenberg.org/files/2638/2638-h/2638-h.htm', 'http://www.gutenberg.org/files/2542/2542-h/2542-h.htm', 'http://www.gutenberg.org/files/11/11-h/11-h.htm', 'http://www.gutenberg.org/files/2701/2701-h/2701-h.htm', 'http://www.gutenberg.org/files/74/74-h/74-h.htm', 'http://www.gutenberg.org/files/76/76-h/76-h.htm' ]
-
-def get_book():
-	url = books[randint(0, len(books)-1)]
-	response = urllib2.urlopen(url)
+def get_book_data(book_url): 
+	response = urllib2.urlopen(book_url)
 	soup = BeautifulSoup(response, 'html.parser')
 	html_as_string = '' 
-	#print html.get_text()
-	for par in soup.find_all('p'):
-		html_as_string += par.get_text()
-	re.sub(u"(\u2018|\u2019)", u"\u201d", "'",'"', html_as_string)
-	title = soup.title.get_text().split(',')[0].strip()
-	author = soup.title.get_text().split(',')[1].strip().replace('by', '')
-	#mc = MarkovChain()
-	#mc.add
-	# print html_as_string
-	return { 'text': html_as_string,
+	paragraphs = soup.find_all('p')
+	for par in paragraphs:
+		html_as_string += par.get_text().encode('utf-8')
+	soup_title = soup.title.get_text().split(',')
+	title = soup_title[0].strip().encode('utf-8')
+	author = soup_title[1].replace("By", "").replace("by", "").strip().encode('utf-8') if soup_title[1].strip().lower().startswith("by") else "Error author name"
+	author = ' '.join(filter(lambda x: re.match("(^\w)", x), author.split()))
+	author_initials =  '.'.join([x[0] for x in author.split(" ")]).encode('utf-8') if author.startswith("Error") == False else "Error author name"
+	passage = paragraphs[randint(0, len(paragraphs)-1)].get_text()
+
+	return { 'text': 'html_as_string',
 			 'title': title,
-			 'author': author	}
-	#return data		 
-	
+			 'author': author,
+			 'author_initials': author_initials,
+			 'passage': passage	 }		 
 
 
 
+#data_fetch_test ref. run file comment to book_list variable
 
-""" 
+		
+
+
+#
+#from cc_markov import MarkovChain
+#print html.get_text()
+#print author
+#print author
+#print author_initials
+#mc = MarkovChain()
+#mc.add
+#print author_initials
+#print books
+#print get_book_data()
+# 'http://www.gutenberg.org/files/11/11-h/11-h.htm', 'http://www.gutenberg.org/files/2701/2701-h/2701-h.htm', 'http://www.gutenberg.org/files/74/74-h/74-h.htm', http://www.gutenberg.org/files/76/76-h/76-h.htm'
+#print get_book_data('http://www.gutenberg.org/files/158/158-h/158-h.htm')
+#http://www.gutenberg.org/files/1184/1184-h/1184-h.htm
+"""
+
+#from io import BytesIO
+#from gtts import gTTS
+#import facebook
+#import requests
+#import ssl 
 token = 'EAADzT03XrgYBACW4m3hvqIlRGecMr0YunnNoTPZBap57IfoGf3JBIZB6BkZCe7Yr50YZBYkomTZCrRcg6mUQPpZAsUE2fWVOUCV0YtvAHdV0V0dMKy5M5xwxlcsk3aFAb0t9pDvfFPK0x8xFJc3DQ2QUgu00XV6vTtHG5GphlHMkpepYF9q7teJRfvo3BOjhOtLmkqZAWq881lq5pMpOQJEARF8AIU6jmWKknBQuiPpzwZDZD'
 
 graph = facebook.GraphAPI(access_token=token, version = 3.1)
